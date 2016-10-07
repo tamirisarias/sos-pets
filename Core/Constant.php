eec21129e3c51486d31b1a491ca7cf447712b37d
williamborba <wborba.dev@gmail.com>
2016-10-07 09:28:11 -0300

    andamento

--- Core/Constant.php
+++ Core/Constant.php
@@ -3,9 +3,6 @@
  * @author Tamiris Arias <tamirisariasjustino@gmail.com>
  * Contants of project
  */
-define('DEBUG',true);
-define('REQUEST_URI',$_SERVER['REQUEST_URI']);
 define('ROOT_PATH',__DIR__);
 define('DATABASE_PATH',ROOT_PATH.'/Config/database.json');
 define('DATABASE','dsv');
-define('QUERY_LIMIT',30);
